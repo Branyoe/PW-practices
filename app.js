9916181689c67fdbe4da7e21b3078c6d3d2f03c6
branyoe <bhernandez1@ucol.mx>
2023-02-08 08:03:03 -0600

    Feat: celsius package is added

---
+++ app.js
@@ -0,0 +1,7 @@
+const { toFahrenheit, toCelsius } = require("celsius");
+// const convertFahrenheitToCelsius = require("celsius");
+// convertFahrenheitToCelsius.toCelsius
+
+const celsius = 36;
+console.log(toFahrenheit(celsius));
+console.log(toCelsius(toFahrenheit(celsius)));
