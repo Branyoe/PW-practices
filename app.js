fe7de99bee3144b1caed06303211f8565f15a324
branyoe <bhernandez1@ucol.mx>
2023-02-08 08:12:44 -0600

    Doc: comments added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const { toFahrenheit, toCelsius } = require("celsius");
-// const convertFahrenheitToCelsius = require("celsius");
-// convertFahrenheitToCelsius.toCelsius
+//**Inyeccion de dependencias */
+const { toFahrenheit, toCelsius } = require("celsius"); //uso de destructuración
+// const convertFahrenheitToCelsius = require("celsius"); //modulo completo
+// convertFahrenheitToCelsius.toCelsius //ejemplo de uso
 
-const celsius = 36;
-console.log(toFahrenheit(celsius));
-console.log(toCelsius(toFahrenheit(celsius)));
+//**Demo*/
+const celsius = 36; //representa grados celsius
+console.log(toFahrenheit(celsius)); //imprime la comversión de celsius a Fahrenheit usando toFahrenheit
+console.log(toCelsius(toFahrenheit(celsius))); //imprime la comversión de Fahrenheit a celsius usando toCelsius
