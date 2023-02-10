988fa1af646b092d9e56e8c489ed2f1d2b18db36
branyoe <bhernandez1@ucol.mx>
2023-02-10 07:46:37 -0600

    feat: ES6 functions

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -1,21 +1,15 @@
-function greet(){
-  console.log('Hello');
-}
+const greet = () => console.log('Hello');
 greet();
 
 //functions as parameterr
-function logGreeting(fn){
-  fn();
-}
+logGreeting = fn => fn();
 logGreeting(greet);
 //Estamos pasando la función greet 
 //como parámetro e invocandola con el 
 //nombre del argumento "fn"
 
 //functions expression
-let greetMe = function(){
-  console.log('Hello from the function expression');
-}
+let greetMe = () => console.log('Hello from the function expression');
 greetMe();
 
 logGreeting(greetMe)
