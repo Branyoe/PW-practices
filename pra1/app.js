92683d6e6ab49fbd36da234246cf6292f43f5cd2
branyoe <bhernandez1@ucol.mx>
2023-02-10 08:26:34 -0600

    refactor: changes in directory strcture

--- pra1/app.js
+++
@@ -1,9 +0,0 @@
-//**Inyeccion de dependencias */
-const { toFahrenheit, toCelsius } = require("celsius"); //uso de destructuración
-// const convertFahrenheitToCelsius = require("celsius"); //modulo completo
-// convertFahrenheitToCelsius.toCelsius //ejemplo de uso
-
-//**Demo*/
-const celsius = 36; //representa grados celsius
-console.log(toFahrenheit(celsius)); //imprime la comversión de celsius a Fahrenheit usando toFahrenheit
-console.log(toCelsius(toFahrenheit(celsius))); //imprime la comversión de Fahrenheit a celsius usando toCelsius
