36bfbe8d82331502c2e88b08a738ed991cdafc07
branyoe <bhernandez1@ucol.mx>
2023-03-01 07:28:34 -0600

    feat: routes uno, prueba was add

--- httpServer/app.js
+++ httpServer/app.js
@@ -5,7 +5,16 @@ const port = 3000;
 
 //Función que define como se comportará la ruta raíz con el metodo GET
 app.get('/', (req, res) => {
-  res.send('Hello World');//response
+  res.send('Hello World, this is the root route');//response
+})
+
+
+app.get('/uno', (req, res) => {
+  res.send('Hello World, from route one');//response
+})
+
+app.get('/prueba', (req, res) => {
+  res.send('Hello World, from route Prueba');//response
 })
 
 //pone a la escucha el servidor en local
