3fb2861a8c3c6343089e7d84ffb3c8a6064c499e
branyoe <bhernandez1@ucol.mx>
2023-03-22 10:25:06 -0600

    refactor: changes in person.ejs and documentation was added

--- httpServer/server.js
+++ httpServer/server.js
@@ -33,8 +33,8 @@ app.get('/', (req, res) => {
 //ruta con paso de parametros
 app.get('/person/:id', (req, res) => {
   res.render('person', { //renderiza person.ejs
-    ID: req.params.id,
-    MESSAGE: req.query.message,
+    ID: req.params.id, //dato proveniente de params
+    MESSAGE: req.query.message, //datos provenientes de query
     TIMES: req.query.times //extrae la query del header
   })
 })
