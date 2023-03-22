199a4efb20b9859cc9e1974e91c2eaeb05f1625f
branyoe <bhernandez1@ucol.mx>
2023-03-22 07:49:34 -0600

    feat: steep1

---
+++ httpServer/server.js
@@ -0,0 +1,50 @@
+const express = require('express'); //modulo para servidores web
+const path = require('path');
+
+const app = express(); //inicializa la app
+
+const port = process.env.PORT || 3000;//toma el puerto por defecto o de lo contrario el 3000
+
+app.use('/assets', express.static(path.join(__dirname, '/public')));
+
+//Función que define como se comportará la ruta raíz con el metodo GET
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>
+      <h1>Hello word</h1>
+      <p>Este es un parrafo y su contenido debe ser azul</p>
+    </body>
+    </html>
+  `);
+})
+
+//ruta con paso de parametros
+app.get('/person/:id', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="/assets/style.css">
+    </head>
+    <body>
+      Person view
+    </body>
+    </html>
+  `);
+})
+
+//pone a la escucha el servidor en local
+app.listen(port, () => console.log(`server on http://localhost:${port}`)); 
