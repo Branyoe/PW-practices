1ee08b6273cd5b167150be06ab7d12172695e02a
branyoe <bhernandez1@ucol.mx>
2023-03-24 08:24:13 -0600

    feat: student and addStudent route was added

--- httpServer/server.js
+++ httpServer/server.js
@@ -6,38 +6,35 @@ const app = express(); //inicializa la app
 const port = process.env.PORT || 3000;//toma el puerto por defecto o de lo contrario el 3000
 
 app.use('/assets', express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true })); //para poder recibir datos de formularios (POST)
 
 app.set('view engine', 'ejs'); //configura ejs como motor de plantillas
 
 //Función que define como se comportará la ruta raíz con el metodo GET
-app.get('/', (req, res) => {
+app.get('/student', (req, res) => {
   //renderiza html plano
-  res.send(` 
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <link rel="stylesheet" href="assets/style.css">
-    </head>
-    <body>
-      <h1>Hello word</h1>
-      <p>Este es un parrafo y su contenido debe ser azul</p>
-    </body>
-    </html>
-  `);
+  res.render('student'); //renderiza student.ejs
 })
 
-//ruta con paso de parametros
-app.get('/person/:id', (req, res) => {
-  res.render('person', { //renderiza person.ejs
-    name: req.params.id, //dato proveniente de params
-    message: req.query.message, //datos provenientes de query
-    times: req.query.times //extrae la query del header
-  })
-})
+// //Función que define como se comportará la ruta addStudent con el metodo POST
+// app.post('/addStudent', (req, res) => {
+//   res.send(`
+//     NSS: ${req.body.nss}
+//     Name: ${req.body.name}
+//     Age: ${req.body.age}
+//     Blood Type: ${req.body.bloodType}
+//   `);
+// });
 
+//Función que define como se comportará la ruta addStudent con el metodo POST
+app.post('/addStudent', (req, res) => {
+  //renderiza una vista ejs con los datos del formulario
+  res.render('displayData', {
+    name: req.body.name,
+    nss: req.body.nss,
+    age: req.body.age,
+    bloodType: req.body.bloodType
+  });
+});
 //pone a la escucha el servidor en local
 app.listen(port, () => console.log(`server on http://localhost:${port}`)); 
