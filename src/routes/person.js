9f6bb6f2a715909933b78906f667a6d0f073698d
branyoe <bhernandez1@ucol.mx>
2023-04-19 07:38:53 -0600

    Feat: person-json endpoind was added

--- src/routes/person.js
+++ src/routes/person.js
@@ -9,5 +9,10 @@ router.get('/person', (req, res) => {
   res.render('person');	//renderiza la vista person como respuesta
 });
 
+router.post('/person-json', express.json({type: '*/*'}), (req, res) => {
+  console.log(`name: ${req.body.name}`);	//imprime en consola el cuerpo de la petición
+  console.log(`name: ${req.body.apellido}`);	//imprime en consola el cuerpo de la petición
+});
+
 //***exportación***
 module.exports = router;
