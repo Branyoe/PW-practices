075ff183453112c0732c5c4b81ca6fd8d2f5df9e
branyoe <bhernandez1@ucol.mx>
2023-05-05 16:59:08 -0600

    feat: addPerson endpoint in POST and GET was added, gente endpoint was sustitute for persons

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const personRouter = require('./routes/persons');
 require('dotenv').config();
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, '/views'));
+
+app.use('/assets', express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 app.use(personRouter);
 
 module.exports = app;
--- src/routes/persons.js
+++ src/routes/persons.js
@@ -3,9 +3,27 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Person = require('../models/Person');
 
-router.get('/gente', async (req, res) => {
-  const Persons = await Person.find({});
-  res.json(Persons);
+router.get('/persons', async (req, res) => {
+  const persons = await Person.find({});
+  res.render('personsView', { persons });
+});
+
+router.get('/add-person', (req, res) => {
+  res.render('addPersonView');
+});
+
+router.post('/add-person', (req, res) => {
+  const newPerson = new Person({
+    name: req.body.name,
+    age: req.body.age,
+    bloodType: req.body.bloodType,
+    nss: req.body.nss
+  });
+
+  newPerson
+  .save()
+  .then(data => res.redirect('/persons'))
+  .catch(err => res.json(err.message));
 });
 
 module.exports = router;
